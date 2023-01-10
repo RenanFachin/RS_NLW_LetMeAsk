fc4ee4ab875c77d6879a4089ff96fbd846e0f917
Renan <renan.fachin@hotmail.com>
2023-01-10 20:21:33 -0300

    Created the base style theme sheet for the project

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,8 @@
-import { useState } from "react"
-
 export function Button() {
-    const [counter, setCounter] = useState<number>(0)
-
-    function increment() {
-        setCounter(counter + 1)
-        console.log(counter)
-    }
 
     return (
-        <button className="bg-violet-600 text-white rounded-lg p-5" onClick={increment}>
-            {counter}
+        <button className="bg-gray-300">
+            
         </button>
     )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,40 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    fontFamily:{
+      Roboto: ['Roboto', 'sans-serif'],
+      Poppins: ['Poppins', 'sans-serif']
+    },
+    colors: {
+      details: '#FEFEFE',
+      black: '#29292E',
+      white: '#f8f8f8',
+      shadow: {
+        500: '#050206'
+      },
+      purple: {
+        500: '#835AFD',
+        hover: '#6F4BD8'
+      },
+      danger: {
+        500: '#E73F5D',
+        hover: '#D73754'
+      },
+      gray: {
+        300:'#DBDCDD',
+        500:'#A8A8B3',
+        800:'#737380'
+      },
+      gray_hover: {
+        300:'#CECECE',
+        500:'#7E7E86',
+      },
+      pink: {
+        300: '#D67EE2',
+        500: '#E559F9'
+      }
+
+    },
     extend: {},
   },
   plugins: [],
